7b4e6da04bdad7d46281d8042bc0c00ec79b8fa5
Leandro Silva <lsilva.info@gmail.com>
2025-11-30 19:21:09 -0300

    Issue #0: [feat](batch-processor): adiciona logging e banner
    Adiciona logging para tempo de execução no BatchProcessor.
    Inclui arquivo de banner para visual alternativo na inicialização.
    Melhora o monitoramento e apresentação do sistema.

--- modules/batch-processor/src/main/java/bio/prodesp/deduplicacao/batch/BatchProcessorApplication.java
+++ modules/batch-processor/src/main/java/bio/prodesp/deduplicacao/batch/BatchProcessorApplication.java
@@ -1,13 +1,21 @@
 package bio.prodesp.deduplicacao.batch;
 
+import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 
+@Log
 @SpringBootApplication
 @EnableBatchProcessing
 public class BatchProcessorApplication {
     public static void main(String[] args) {
+
+        log.info(":: Iniciando Deduplicação-Batch ::");
+        long startTime = System.currentTimeMillis(); // Captura o tempo de início
         SpringApplication.run(BatchProcessorApplication.class, args);
+        long endTime = System.currentTimeMillis(); // Captura o tempo de fim
+        long totalTime = endTime - startTime; // Calcula o tempo total em milissegundos
+        log.info(":: Deduplicação-Batch iniciado com sucesso :: - " + totalTime + " ms" );
     }
 }
